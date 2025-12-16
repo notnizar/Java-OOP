c4019598e55582ac99d0e9420f30cf2cc1efb77d
Nizar Zaid <nezarzaid472@gmail.com>
2025-12-16 23:00:15 +0300

    Added Search By Name Function

--- Java/Book Mangment System/Book.java
+++ Java/Book Mangment System/Book.java
@@ -13,6 +13,32 @@ public class Book {
         this.price = price;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    
+
+
     @Override
     public String toString() {
         return "Book{" + "title=" + title + ", author=" + author + ", price=" + price + '}';
--- Java/Book Mangment System/Library.java
+++ Java/Book Mangment System/Library.java
@@ -1,28 +1,37 @@
-
-
 package com.mycompany.mavenproject1;
 
 public class Library {
-    private Book[] books;
-    
-    public Library(int size){
+
+    private final Book[] books;
+
+    public Library(int size) {
         this.books = new Book[size];
     }
-    
-public void addBook(Book newBook) {
-    boolean isAdded = false;
 
-    for (int i = 0; i < books.length; i++) {
-        if (books[i] == null) {
-            books[i] = newBook;
-            isAdded = true;
-            System.out.println("Book added successfully: ");
-            break;
+    public void addBook(Book newBook) {
+        boolean isAdded = false;
+
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] == null) {
+                books[i] = newBook;
+                isAdded = true;
+                System.out.println("Book added successfully: ");
+                break;
+            }
+        }
+
+        if (isAdded == false) {
+            System.out.println("Sorry, Library is full!");
         }
     }
 
-    if (isAdded == false) {
-        System.out.println("Sorry, Library is full!");
+    public Book searchByTitle(String title) {
+        for (Book book : books) {
+            if (book != null && book.getTitle().equals(title)) {
+                return book;
+            }
+
+        }
+        return null;
     }
-}
 }
