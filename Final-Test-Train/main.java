d160495de819cbfd626e73edab178c25c0ddbb08
Nizar <nezarzaid472@gmail.com>
2026-01-07 16:16:04 +0300

    feat: Added  main class to test the project

---
+++ Final-Test-Train/main.java
@@ -0,0 +1,29 @@
+package com.mycompany.programmingcontest;
+
+import java.util.Scanner;
+
+public class main {
+
+    public class Main {
+
+        public static void main(String[] args) {
+
+            ProgrammingContest contest = new ProgrammingContest();
+
+            for (int i = 0; i < 4; i++) {
+                System.out.println("Entering data for Team " + (i + 1));
+                contest.addTeam();
+            }
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter member Data: ");
+            int limit = scanner.nextInt();
+
+            for (Team t : contest.getTeams()) {
+                if (t.getMembers() <= limit) {
+                    System.out.println(t.getTeaminfo());
+                }
+            }
+        }
+    }
+}
